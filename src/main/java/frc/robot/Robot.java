2962f1e590206eeb822ffb02152d5c24b7652e3d
kevinbai0 <kevin.c.bai0@gmail.com>
2019-01-12 15:16:56 -0500

    speedy quick

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,19 +11,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 
 //Kevinlikesbiggay
 
 public class Robot extends TimedRobot {
-	int motor = 0;
-	int kevingay = 1;
-	int calebcewl = 2;
+	int motor = 0; //frontright 
+	int kevingay = 1;//backright
+	int calebcewl = 2;//backleft
 	int kevingaymore = 3;
 	Spark sparkmotor;
 	Spark sparkkevingay;
@@ -52,22 +47,15 @@ public class Robot extends TimedRobot {
 		//sparkmotor.setInverted(true);
 		//sparkcalebcewl.setInverted(true);
 
-		
 		right = new SpeedControllerGroup(sparkkevingay, sparkmotor);
 		left = new SpeedControllerGroup(sparkcalebcewl,sparkkevingaymore);
 		
 
-		
+		right.setInverted(true);
 		
 		joystick = new Joystick(0);
 	}	
 	
-
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
 	public void disabledInit() {
 
@@ -82,14 +70,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void autonomousInit() {
 
-
-		
-
 	}
-
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -111,31 +92,31 @@ public class Robot extends TimedRobot {
 		double y = -joystick.getRawAxis(1);
 		double x = joystick.getRawAxis(0);
 
-		double leftSpeed = y*0.7;
-		double rightSpeed = y*-0.7;
+		double leftSpeed = y;
+		double rightSpeed = y;
 		
 		if (x > 0.5) {
-			rightSpeed = -x*0.4;
-			
-
+			rightSpeed = -x*0.9;
 		}
 		else if (x < -0.5) {
-			leftSpeed = x*0.4;
+			leftSpeed = x*0.9;
 		}
 
 		if(-0.3 <y && 0.3>y){
-			System.out.println("y is between -0.3 and 0.3");
+			//System.out.println("y is between -0.3 and 0.3");
 			if(x<-0.5){
-				System.out.println("turn left");
-				right.set(0.5);
-				left.set(-0.5);
+				//System.out.println("turn left");
+				rightSpeed = 0.5;
+				leftSpeed = -0.5;
 			}
 			else if(x>0.5){
-				System.out.println("turn right");
-				left.set(0.5);
-				right.set(-0.5);
+				//System.out.println("turn right");
+				leftSpeed = 0.5;
+				rightSpeed = -0.5;
 			}
 		}
+
+		System.out.println(leftSpeed + " " + rightSpeed);
 
 		left.set(leftSpeed);
 		right.set(rightSpeed);
@@ -144,13 +125,7 @@ public class Robot extends TimedRobot {
 
 	}	
 	
-	
-	
-	
-	
-	/**
-	 * This function is called periodically during test mode.
-	 */
+
 	@Override
 	public void testPeriodic() {
 	}
